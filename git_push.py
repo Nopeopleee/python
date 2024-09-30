ad2e98385b459788f6e61f9b3b637e2301191a89
HiiiEricWu <eric.wu@hiii.com.tw>
2024-09-30 12:48:36 +0800

    commit message

--- git_push.py
+++ git_push.py
@@ -1,7 +1,10 @@
 import git
 import os
+import argparse
+from datetime import datetime
 
-def git_push():
+
+def git_push(commit_message):
     try:
         repo = git.Repo(os.getcwd())
         repo.git.add(A=True)
@@ -19,4 +22,13 @@ def git_push():
     except Exception as e:
         print(f"An error occurred: {e}")
 
-git_push()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Push changes to git repository.")
+    parser.add_argument("commit_message", type=str, help="The commit message for the changes.")
+    args = parser.parse_args()
+    
+    # 取得當前時間並格式化
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    full_commit_message = f"{args.commit_message} - {current_time}"
+    
+    git_push(full_commit_message)
