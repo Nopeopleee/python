2b2f1ce5aab164b3ab4284e0123a6b02cedfa9e9
HiiiEricWu <eric.wu@hiii.com.tw>
2024-09-30 12:58:38 +0800

    commit - 2024-09-30 12:58:38

--- git_push.py
+++ git_push.py
@@ -4,14 +4,15 @@ import argparse
 from datetime import datetime
 
 
-def git_push(commit_message):
+def git_push(commit_message, branch="main"):
     try:
         repo = git.Repo(os.getcwd())
         repo.git.add(A=True)
         print(commit_message)
         repo.index.commit(commit_message)
+        print(f"Changes committed to {branch} branch.")
         origin = repo.remote(name='origin')
-        push_info = origin.push(refspec='main')
+        push_info = origin.push(refspec=branch)
         
         for info in push_info:
             if info.flags & info.ERROR:
@@ -26,10 +27,11 @@ def git_push(commit_message):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Push changes to git repository.")
     parser.add_argument("commit_message", type=str, nargs='?', default="commit", help="The commit message for the changes.")
+    parser.add_argument("--branch", type=str, default="main", help="The branch to push the changes to.")
     args = parser.parse_args()
     
     # 取得當前時間並格式化
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     full_commit_message = f"{args.commit_message} - {current_time}"
     
-    git_push(full_commit_message)
+    git_push(full_commit_message, args.branch)
