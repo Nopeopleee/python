a5a24bd318863f888d9562929ab8e8e97220921d
Nopeopleee <t896057895@gmail.com>
2024-11-04 22:10:44 +0800

    random string generator - 2024-11-04 22:10:44

---
+++ random_string.py
@@ -0,0 +1,14 @@
+import random
+import string
+import argparse
+
+def random_string(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(length))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate a random string.")
+    parser.add_argument("length", type=int, help="The length of the random string to generate.")
+    args = parser.parse_args()
+    
+    print(random_string(args.length))
